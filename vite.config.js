0586f5f4d4cf5a4e55b1a65702d2cded4a350975
Mustapha377 <143838046+Mustapha377@users.noreply.github.com>
2024-12-25 22:57:12 +0100

    react and react-dom install

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
    plugins: [react()],
- base: '/Todo-list-app/'
+ base: '/Todo-list-app/',
+ build: {
+  rollupOptions: {
+    external: ['react/jsx-runtime']
+  }
+}
 })
